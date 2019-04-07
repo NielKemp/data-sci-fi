29abb96e20edb5d9fe4e1a8ca72675aa24af42bc
NielKemp <nielqi@gmail.com>
2019-04-07 20:50:58 +0200

    data prepped ready for lesson 3

--- lesson 1/lesson1Code.R
+++ lesson 1/lesson1Code.R
@@ -1,10 +1,34 @@
-dir.create('lesson 1/rawData')
+#dir.create('lesson 1/rawData')
 
-download.file(url = "http://files.grouplens.org/datasets/movielens/ml-latest-small.zip", destfile = "lesson 1/rawData/ml-latest-small.zip")
-unzip("lesson 1/rawData/ml-latest-small.zip",exdir = "lesson 1/rawData")
+#download.file(url = "http://files.grouplens.org/datasets/movielens/ml-latest-small.zip", destfile = "lesson 1/rawData/ml-latest-small.zip")
+#unzip("lesson 1/rawData/ml-latest-small.zip",exdir = "lesson 1/rawData")
 
 
-movies <- read.csv("lesson1/rawData/ml-latest-small/movies.csv")
-ratings <- read.csv("lesson1/rawData/ml-latest-small/ratings.csv")
-tags <- read.csv("lesson1/rawData/ml-latest-small/tags.csv")
-links <- read.csv("lesson1/rawData/ml-latest-small/links.csv")
+#movies <- read.csv("lesson 1/rawData/ml-latest-small/movies.csv")
+#ratings <- read.csv("lesson 1/rawData/ml-latest-small/ratings.csv")
+#tags <- read.csv("lesson 1/rawData/ml-latest-small/tags.csv")
+#links <- read.csv("lesson 1/rawData/ml-latest-small/links.csv")
+
+#save(links, movies, ratings, tags, file = "lesson 1/rawData/movielens-small.Rdata")
+
+#rm(list = ls())
+load("lesson 1/rawData/movielens-small.Rdata")
+
+
+library(tidyverse)
+library(rlang)
+
+
+
+
+ratings %>% group_by(userId)%>%summarize(count=n()) %>%arrange(desc(count))
+
+users_frq <- ratings %>% group_by(userId) %>% summarize(count=n()) %>% arrange(desc(count))
+my_users <-users_frq$userId[101:115]
+
+movies_frq <-ratings%>%group_by(movieId)%>%summarize(count=n())%>%arrange(desc(count))
+my_movies <- movies_frq$movieId[101:120]
+
+ratings_red <-ratings%>%filter(userId %in% my_users,movieId %in% my_movies)
+
+install.packages("rlang", type = "source")
