0e01bd28c1a9e40c1bbf11e6d1ce0ec921bcda15
NielKemp <nielqi@gmail.com>
2019-07-03 20:35:18 +0200

    Finished lesson 4 code, added link at top of code for more examples at keras.rstudio

--- lesson 4/lesson4.R
+++ lesson 4/lesson4.R
@@ -1,3 +1,6 @@
+
+#LOTS OF EXAMPLES HERE: https://keras.rstudio.com/articles/examples/index.html
+
 #install keras R package from CRAN
 install.packages("keras")
 
@@ -119,3 +122,106 @@ model3 %>% fit(x_train,y_train,epochs=100,batch_size=32)%>%plot()
 
 #evaluate
 model3%>%evaluate(x_test,y_test,batch_size=32,verbose=1)
+
+
+#More neural nets, this time on MNIST data
+library(imager) #has many useful tools for image processing
+
+#read Mnist data into object
+mnist <- dataset_mnist() #comes built into the Keras package
+head(mnist)
+
+#xTrain/xTest are 3-d arrays(images,width, height) of greyscale values.
+x_train <- mnist$train$x
+y_train <- mnist$train$y
+
+x_test <- mnist$test$x
+y_test <- mnist$test$y
+
+#plot a few of the images
+par(mfrow =c(1,5))
+for( i in 1:5){plot(as.cimg(t(x_test[i,,])))}
+
+#treat each pixel independently, by unvravelling the 28x28 matrix into a vector of length 784 
+#this means that each row will be a different pixel of the image
+#the 0 to 255 greyscale encoding will be converted down to a range of 0 to 1
+
+#reshape matrix into vector
+dim(x_train) <- c(nrow(x_train),784)
+dim(x_test) <- c(nrow(x_test),784)
+
+head(x_train)
+
+#rescale
+x_train <- x_train/255
+x_test <- x_test/255
+
+#check current target variable
+head(y_train)
+head(y_test)
+
+#change targets into:
+# 0 -> [1,0,0,0,0,0,0,0,0]
+# 1 -> [0,1,0,0,0,0,0,0,0]
+# 2 -> [0,0,1,0,0,0,0,0,0] etc
+
+#do this using 1-hot encoding
+#one-hot encoding is implemented using to_categorical() in Keras
+y_train <- to_categorical(y_train,10)
+y_test <-to_categorical(y_test,10)
+
+head(y_train)
+head(y_test)
+
+#create model
+model <- keras_model_sequential()
+
+#define model
+#fit a 2 h-layer model with dropout
+#1st layer -> 128 neurons, 2nd layer -> 64 neurons. Both using relu activation functions
+#use softmax activation in output layer
+#softmax has the properties that it forces the outputs to sum up to 1, so it's perfect for probabilities
+#predicted class is the class with the highest predicted probability
+
+model %>%
+  layer_dense(units=128, activation ='relu', input_shape =c(784))%>%
+  layer_dropout(rate=0.4)%>%
+  layer_dense(units=64,activation='relu')%>%
+  layer_dropout(rate=0.3)%>%
+  layer_dense(units=10,activation='softmax')
+
+#check model summary
+summary(model)
+
+#compile the model
+model %>% compile(
+  loss='categorical_crossentropy',
+  optimizer='rmsprop',
+  metrics=c('accuracy')
+  )
+
+#train the model 
+
+history <- model %>% fit(x_train,y_train,
+                         epochs=3, batch_size = 32,
+                         validation_split = 0.2#add this only to training for hyperparam tuning
+                         )
+
+plot(history)
+
+#evaluate the model
+model %>% evaluate(x_test,y_test)
+
+#look at some images the model predicted incorrectly
+preds <- model %>% predict_classes(x_test)
+
+wrong_preds <- tibble(id = 1:nrow(y_test),
+                      obs = mnist$test$y,
+                      preds=preds)%>%
+  filter(obs!=preds)
+
+par(mfrow = c(1:5))
+for(i in 1:5){
+            wrong_x <- mnist$test$x[wrong_preds$id[i],,]
+            plot(as.cimg(t(wrong_x)))
+            }
